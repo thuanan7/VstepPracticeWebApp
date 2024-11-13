c352b00b8856e42b1aeca557438f97d70daffde4
thuanan7 <thuanan7@gmail.com>
2024-11-13 16:02:01 +0700

    Update mapper profile

--- VstepPractice.API/Mapper/UserMappingProfile.cs
+++ VstepPractice.API/Mapper/UserMappingProfile.cs
@@ -6,6 +6,8 @@ using VstepPractice.API.Models.DTOs.Users.Responses;
 using VstepPractice.API.Models.DTOs.Exams.Responses;
 using VstepPractice.API.Models.DTOs.Sections.Responses;
 using VstepPractice.API.Models.DTOs.Questions.Responses;
+using VstepPractice.API.Models.DTOs.Passage.Responses;
+using VstepPractice.API.Models.DTOs.SectionParts.Responses;
 
 namespace VstepPractice.API.Mapper;
 
@@ -17,12 +19,12 @@ public class UserMappingProfile : Profile
         CreateMap<PagedResult<User>, PagedResult<UserDto>>().ReverseMap();
 
         CreateMap<CreateUserDto, User>().ReverseMap();
-        CreateMap<UpdateUserDto, User>()
-            .ReverseMap()
-            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+        CreateMap<UpdateUserDto, User>().ReverseMap();
 
         CreateMap<Exam, ExamResponse>().ReverseMap();
         CreateMap<Section, SectionResponse>().ReverseMap();
+        CreateMap<SectionPart, SectionPartResponse>().ReverseMap();
+        CreateMap<Passage, PassageResponse>().ReverseMap();
         CreateMap<Question, QuestionResponse>().ReverseMap();
         CreateMap<QuestionOption, QuestionOptionResponse>().ReverseMap();
     }
